8ce0e44b46f5efe63df7805a8278d573edf0fb11
Faltsu <46961449+Faltsu@users.noreply.github.com>
2019-09-13 15:06:37 +0300

    Add files via upload

---
+++ workshop2.js
@@ -0,0 +1,16 @@
+<script>
+function repeatingText() {
+for (var i=0; i<50; i++){document.write("repeatingText<br>")};
+}
+</script>
+
+<script>
+function browser() {
+if (navigator.appCodeName="Mozilla"){
+location.href="http://mozilla.org"
+}
+else{
+location.href="http://www.microsoft.com";
+}
+}
+</script>
